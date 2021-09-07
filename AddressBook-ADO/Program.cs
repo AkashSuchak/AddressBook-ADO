079b8fc7effcf2d8850013d3e4e78df73a9d4137
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-07 23:23:54 +0530

    Add project files.

---
+++ AddressBook-ADO/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook_ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book ADO.Net");
+            Console.WriteLine("===============================");
+        }
+    }
+}
