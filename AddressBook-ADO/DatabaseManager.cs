00a4d5988c1822847b8555e33e06b458403b7245
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-08 02:06:04 +0530

    feat: Added: Create Database:AdressBook

---
+++ AddressBook-ADO/DatabaseManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBook_ADO
+{
+    class DatabaseManager
+    {
+        //UC1
+        public void CreateDatabase()
+        {
+            //Sql Connection
+            SqlConnection myConn = new SqlConnection(@"Data Source=(LocalDb)\localdb;Integrated Security=True");
+
+            //Sql Query to Create Databse
+            String str = "CREATE DATABASE AddressBook ";
+
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+            try
+            {
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+                Console.WriteLine("DataBase is Created Successfully");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                {
+                    myConn.Close();
+                }
+            }
+        }
+    }
+}
--- AddressBook-ADO/Program.cs
+++ AddressBook-ADO/Program.cs
@@ -6,8 +6,13 @@ namespace AddressBook_ADO
     {
         static void Main(string[] args)
         {
+            //Display Welcome Message
             Console.WriteLine("Welcome to Address Book ADO.Net");
             Console.WriteLine("===============================");
+
+            DatabaseManager manager = new DatabaseManager();
+            //UC1
+            manager.CreateDatabase();
         }
     }
 }
